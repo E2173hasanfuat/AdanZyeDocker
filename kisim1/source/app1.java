cc084867342672d0d0190b75dd030b7024110c91
Hasan Akdag <hasanfuatakdag@gmail.com>
2025-02-20 10:39:22 +0100

    deneme5

--- kisim1/source/app1.java
+++ kisim1/source/app1.java
@@ -1,6 +1,6 @@
 public class app1 {
     public static void main(String[] args) {
-        System.out.println("Merhaba arkadaslar ben bir App1 uygulamasiyim...");
+        System.out.println("Merhaba arkadaslar ben bir App1 uygulamasiyim.....");
 
     }
 }
